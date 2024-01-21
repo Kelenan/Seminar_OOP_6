87090fd806e05615d436648d18a0c3654e6f6293
«Elena <leseee4ka@bk.ru>
2024-01-21 13:22:52 +0300

    1

---
+++ src/homework/SaveUser.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class SaveUser implements Saveable{
+    private  final User user;
+    public SaveUser(User user){
+        this.user = user;
+    }
+    @Override
+    public void save() {
+        System.out.println("Save user: " + user.getName());
+    }
+}
